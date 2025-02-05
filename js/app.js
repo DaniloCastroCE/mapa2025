1958b3a1176b0ccf4a17c9fb0c191a67d371d22b
DaniloCastroCE <38533079+DaniloCastroCE@users.noreply.github.com>
2025-02-05 20:53:41 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const loading = new Loading('loading')
 const mapa = new Mapa([-3.74565, -38.51723], 14, new GeoJson())
 const locais = new Local()
+const toast = new Toast()
 var estado = ''
 var bairros = true
 const dVisivel = "M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-146 0-266-81.5T40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200Zm0-300Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z"
@@ -327,12 +328,14 @@ const deletaItemLista = (index) => {
 const onclickCopy = (idMarker) => {
     const local = locais.getLocalIdMarker(idMarker)
     let copy =
-        `*${local.nome}*
+`*${local.nome}*
 Endereço: ${local.end.rua}, ${local.end.num} - ${local.end.bairro}
 `
     navigator.clipboard.writeText(copy)
     mapa.focoMarker(mapa.getMarker(idMarker))
-
+    toast.showToast(`Copiado:<br>
+                    *${local.nome}*<br>
+                    Endereço: ${local.end.rua}, ${local.end.num} - ${local.end.bairro}`)
 }
 
 const copyAll = () => {
